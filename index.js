cc8c7d6f57f2b0323e14b96b59313206a91396ab
NickWang <brotherstyx@gmail.com>
2021-08-25 14:00:49 +0800

    feat: 🎸 use updated api

---
+++ index.js
@@ -0,0 +1,7 @@
+const protoLoader = require('@grpc/proto-loader');
+const grpcLibrary = require('grpc');
+
+protoLoader.load('./test.proto', {}).then(packageDefinition => {
+  const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
+  console.log(packageObject);
+});
